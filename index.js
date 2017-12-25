9cff0b7e5a61d22683e91c4f2ea161850304c19e
Laura Luidolt <laura.luidolt@gmail.com>
2017-12-25 11:50:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,19 @@ function destructivelyRemoveLastKitten(){
 function destructivelyRemoveFirstKitten(){
   kittens.shift(1);
 }
+
+function appendKitten(name){
+  return [...kittens, name];
+}
+
+function prependKitten(name){
+  return [name, ...kittens];
+}
+
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length-1);
+}
+
+function removeFirstKitten(){
+  kittens.slice(1);
+}
